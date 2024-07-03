027da7b54d83c33b3ddda2e879764d32c4b08ee4
Gustavo Larco <gustavo.larcoj@gmail.com>
2024-07-03 15:58:08 -0500

    final

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -14,7 +14,7 @@ const ContactPage = () => {
                 <div className="max-w-[450px]">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">My <span className="font-bold text-secondary"> information.</span></h1>
-                    <p className="mb-3 text-xl text-gray-300">Please for any information or request for collaborations in projects, work and any kind of services, don't hessitate to contact me at the next ways.</p>
+                    <p className="mb-3 text-xl text-gray-300">Please for any information or request for collaborations in projects, work and any kind of services, do not hessitate to contact me at the next ways.</p>
                 </div>
 
                 {/* SLIDER */}
