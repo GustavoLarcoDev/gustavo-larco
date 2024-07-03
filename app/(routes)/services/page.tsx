dc4392306ba23b0be8041f616ee32080a4e19864
Gustavo Larco <gustavo.larcoj@gmail.com>
2024-07-03 15:58:54 -0500

    The end

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -14,7 +14,7 @@ const ServicesPage = () => {
                 <div className="max-w-[450px]">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">My <span className="font-bold text-secondary"> services.</span></h1>
-                    <p className="mb-3 text-xl text-gray-300">I offer frontend web development services specializing in creating attractive and functional websites and applications. Using the latest technologies, such as HTML, CSS, and JavaScript, I design intuitive and responsive user interfaces that reflect my clients' brand identity and enhance their online presence.</p>
+                    <p className="mb-3 text-xl text-gray-300">I offer frontend web development services specializing in creating attractive and functional websites and applications. Using the latest technologies, such as HTML, CSS, and JavaScript, I design intuitive and responsive user interfaces that reflect my clients brand identity and enhance their online presence.</p>
                     <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contact with me</button>
                 </div>
 
